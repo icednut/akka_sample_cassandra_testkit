8b827c61193a7b4468f521feff8ec366e3d67a08
Will <will.109@kakaopaycorp.com>
2020-02-19 17:24:18 +0900

    initial commit

---
+++ src/main/scala/io/icednut/cassandra/testkit/MainApp.scala
@@ -0,0 +1,18 @@
+package io.icednut.cassandra.testkit
+
+import java.io.File
+
+import akka.persistence.cassandra.testkit.CassandraLauncher
+
+/**
+ * @author will.109
+ * @date 2020/02/19
+ **/
+object MainApp extends App {
+  val databaseDirectory = new File("target/cassandra-db")
+  CassandraLauncher.start(databaseDirectory, CassandraLauncher.DefaultTestConfigResource, clean = false, port = 9042)
+
+  sys.addShutdownHook {
+    CassandraLauncher.stop()
+  }
+}
